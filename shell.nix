27b4d0d37841f70ae415cf791f8867063384f8de
xannyx <xander@xvproductions.com>
2025-01-29 21:34:51 +0100

    Created nix-shell for i386

--- shell.nix
+++ shell.nix
@@ -1,25 +1,27 @@
-{pkgs ? import <nixpkgs> {} }:
+{
+  pkgs ? import <nixpkgs> { },
+}:
 let
-pkgs = import <nixpkgs> { 
-	crossSystem = { 
-		config = "i686-elf"; 
-		system = "i686-unknown-none";
-	};
-};
+  pkgs = import <nixpkgs> {
+    crossSystem = {
+      config = "i386-elf";
+      system = "i686-unknown-none";
+    };
+  };
 in
 pkgs.mkShell {
-	nativeBuildInputs = with pkgs.buildPackages; [
-		gcc
-			binutils
-			gnumake
-			nasm
-			grub2
-			xorriso
-	];
+  nativeBuildInputs = with pkgs.buildPackages; [
+    gcc
+    binutils
+    gnumake
+    nasm
+    grub2
+    xorriso
+  ];
 
-	shellHook = ''
-		export TARGET=i686-elf
-		echo "OS Development Environment Ready"
-		echo "Target architecture: $TARGET"
-		'';
+  shellHook = ''
+    		export TARGET=i386-elf
+    		echo "OS Development Environment Ready"
+    		echo "Target architecture: $TARGET"
+    		'';
 }
