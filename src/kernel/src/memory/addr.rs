88519ca60108078915af1b02348f6c6b6ca87415
xannyx <xander@xvproductions.com>
2025-04-16 15:42:18 +0200

    Made a first attempt of a SlabCahce

--- src/kernel/src/memory/addr.rs
+++ src/kernel/src/memory/addr.rs
@@ -1,4 +1,7 @@
-use core::ops::{Add, Sub};
+use core::{
+	isize,
+	ops::{Add, Sub},
+};
 
 /// Represents a physical memory address, wrapping a `usize`.
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
@@ -86,6 +89,56 @@ impl PhysAddr {
 	pub fn as_mut_ptr<T>(self) -> *mut T {
 		return core::ptr::with_exposed_provenance_mut(self.0);
 	}
+
+	/// Aligns the physical address upwards to the given alignment.
+	///
+	/// See the `align_up` function for more information.
+	///
+	/// # Panics
+	///
+	/// This function panics if the resulting address has a bit in the range 52
+	/// to 64 set.
+	#[inline]
+	pub fn align_up<U>(self, align: U) -> Self
+	where
+		U: Into<usize>,
+	{
+		return PhysAddr::new(align_up(self.0, align.into()));
+	}
+
+	/// Aligns the physical address downwards to the given alignment.
+	///
+	/// See the `align_down` function for more information.
+	#[inline]
+	pub fn align_down<U>(self, align: U) -> Self
+	where
+		U: Into<usize>,
+	{
+		return self.align_down_usize(align.into());
+	}
+
+	/// Aligns the physical address downwards to the given alignment.
+	///
+	/// See the `align_down` function for more information.
+	#[inline]
+	pub(crate) const fn align_down_usize(self, align: usize) -> Self {
+		return PhysAddr(align_down(self.0, align));
+	}
+
+	/// Checks whether the physical address has the demanded alignment.
+	#[inline]
+	pub fn is_aligned<U>(self, align: U) -> bool
+	where
+		U: Into<usize>,
+	{
+		return self.is_aligned_usize(align.into());
+	}
+
+	/// Checks whether the physical address has the demanded alignment.
+	#[inline]
+	pub(crate) const fn is_aligned_usize(self, align: usize) -> bool {
+		return self.align_down_usize(align).as_usize() == self.as_usize();
+	}
 }
 
 /* -------------------------------------- */
@@ -122,7 +175,6 @@ impl Add<usize> for VirtAddr {
 	#[inline]
 	#[allow(clippy::expect_used)]
 	fn add(self, rhs: usize) -> Self::Output {
-		// Use constructor for type safety
 		return VirtAddr::new(
 			self.0.checked_add(rhs).expect("VirtAddr add overflow"),
 		);
@@ -155,6 +207,17 @@ impl VirtAddr {
 		return VirtAddr(addr);
 	}
 
+	/// Creates a new canonical virtual address, throwing out bits 24..32.
+	///
+	/// This function performs sign extension of bit 47 to make the address
+	/// canonical, overwriting bits 48 to 64. If you want to check whether an
+	/// address is canonical, use [`new`](Self::new) or
+	/// [`try_new`](Self::try_new).
+	#[inline]
+	pub const fn new_truncate(addr: usize) -> VirtAddr {
+		return VirtAddr(((addr << 8) as isize >> 8) as usize);
+	}
+
 	/// Returns the underlying `usize` representation of the virtual address.
 	#[inline]
 	#[must_use]
@@ -170,11 +233,95 @@ impl VirtAddr {
 		return core::ptr::with_exposed_provenance(self.0);
 	}
 
+	/// Aligns the virtual address upwards to the given alignment.
+	///
+	/// See the `align_up` function for more information.
+	///
+	/// # Panics
+	///
+	/// This function panics if the resulting address is higher than
+	/// `0xffff_ffff_ffff_ffff`.
+	#[inline]
+	pub fn align_up<U>(self, align: U) -> Self
+	where
+		U: Into<usize>,
+	{
+		let aligned_addr = align_up(self.0, align.into());
+
+		return VirtAddr::new(aligned_addr);
+	}
+
+	/// Aligns the virtual address downwards to the given alignment.
+	///
+	/// See the `align_down` function for more information.
+	#[inline]
+	pub fn align_down<U>(self, align: U) -> Self
+	where
+		U: Into<usize>,
+	{
+		return self.align_down_usize(align.into());
+	}
+
 	/// Converts the virtual address into a raw mutable pointer of type `T`.
 	/// Note: Dereferencing requires `unsafe`.
 	#[inline]
 	#[must_use]
 	pub fn as_mut_ptr<T>(self) -> *mut T {
 		return core::ptr::with_exposed_provenance_mut(self.0);
+	}
+
+	/// Aligns the virtual address downwards to the given alignment.
+	///
+	/// See the `align_down` function for more information.
+	#[inline]
+	pub(crate) const fn align_down_usize(self, align: usize) -> Self {
+		return VirtAddr::new_truncate(align_down(self.0, align));
+	}
+
+	/// Checks whether the virtual address has the demanded alignment.
+	#[inline]
+	pub fn is_aligned<U>(self, align: U) -> bool
+	where
+		U: Into<usize>,
+	{
+		return self.is_aligned_usize(align.into());
+	}
+
+	/// Checks whether the virtual address has the demanded alignment.
+	#[inline]
+	pub(crate) const fn is_aligned_usize(self, align: usize) -> bool {
+		return self.align_down_usize(align).as_usize() == self.as_usize();
+	}
+}
+
+/// Align address downwards.
+///
+/// Returns the greatest `x` with alignment `align` so that `x <= addr`.
+///
+/// Panics if the alignment is not a power of two.
+#[inline]
+pub const fn align_down(addr: usize, align: usize) -> usize {
+	assert!(align.is_power_of_two(), "`align` must be a power of two");
+	return addr & !(align - 1);
+}
+
+/// Align address upwards.
+///
+/// Returns the smallest `x` with alignment `align` so that `x >= addr`.
+///
+/// Panics if the alignment is not a power of two or if an overflow occurs.
+#[inline]
+pub const fn align_up(addr: usize, align: usize) -> usize {
+	assert!(align.is_power_of_two(), "`align` must be a power of two");
+	let align_mask = align - 1;
+	if addr & align_mask == 0 {
+		return addr; // already aligned
+	} else {
+		// FIXME: Replace with .expect, once `Option::expect` is const.
+		if let Some(aligned) = (addr | align_mask).checked_add(1) {
+			return aligned;
+		} else {
+			panic!("attempt to add with overflow")
+		}
 	}
 }
